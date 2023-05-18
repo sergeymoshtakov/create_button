4ce45bb6d5f49d66517c6e0b98ae43ccd90c0aad
sergeymoshtakov <113889721+sergeymoshtakov@users.noreply.github.com>
2023-05-18 10:10:09 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Point startPoint;
+        private Button currentButton;
+        public static int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                i++;
+                startPoint = e.Location;
+                currentButton = new Button();
+                currentButton.Text = Convert.ToString("button " + i);
+                currentButton.FlatStyle = FlatStyle.Flat;
+                currentButton.FlatAppearance.BorderSize = 1;
+                currentButton.BackColor = Color.LightGray;
+                Controls.Add(currentButton);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (currentButton != null)
+            {
+                int x = Math.Min(startPoint.X, e.Location.X);
+                int y = Math.Min(startPoint.Y, e.Location.Y);
+                int width = Math.Abs(startPoint.X - e.Location.X);
+                int height = Math.Abs(startPoint.Y - e.Location.Y);
+
+                currentButton.Location = new Point(x, y);
+                currentButton.Size = new Size(width, height);
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && currentButton != null)
+            {
+                currentButton.Click += Button_Click;
+                currentButton = null;
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            MessageBox.Show("Clicked the dynamic button at location: " + clickedButton.Location);
+        }
+    }
+}
